8392a2ca0f997c80a6b60cb5723c959a69e1d573
Gaurav Raut <zgaurav713@gmail.com>
2024-12-20 19:53:08 +0530

    form, gameboard, gameController

--- assets/script.js
+++ assets/script.js
@@ -1,63 +1,89 @@
-const Modal = (() => {
-  const modal = document.getElementById("formdialog");
-  const start = document.getElementById("submitstartgame");
-  const gameForm = document.getElementById("gameform");
+const form = (() => {
+  const startGameButton = document.getElementById("submitstartgame");
+  const form = document.getElementById("controls");
+  const heading = document.getElementById("heading");
 
-  window.addEventListener("DOMContentLoaded", () => {
-    modal.showModal();
-  });
-
-  const openModal = () => {
-    modal.showModal();
+  const formDone = () => {
+    form.remove();
   };
 
-  start.addEventListener("click", (event) => {
+  startGameButton.addEventListener("click", (event) => {
     event.preventDefault();
-    if(!gameForm.checkValidity()){
-      alert("please fill out the form!");
+    if (!form.checkValidity()) {
+      alert("Add your details first!");
       return;
     }
-    gameForm.remove();
-    modal.close();
+
+    gameController.getPlayers();
+    heading.innerText = `${players[0].name.toUpperCase()}  v/s  ${players[1].name.toUpperCase()}`;
+    gameBoard.render();
+
+    formDone();
   });
 
-  const closeModal = () => {
-    modal.close();
-  };
-
-  return { openModal, closeModal };
+  return { formDone };
 })();
 
-const playerFactory=function(name, marker){
-  
-}
-
+//stores state of board, display board and updates board
 const gameBoard = (() => {
-  const gameContainer = document.getElementById("gameboard");
-  let gameBoardArray = ["X", "", "O", "X", "", "", "", "g", ""];
+  const boardContainer = document.getElementById("gameboard");
+  let gameBoardArray = ["", "", "", "", "", "", "", "", ""];
   const rows = 3;
   const cols = 3;
 
-  const render = function (gameBoardArray) {
+  const render = () => {
+    boardContainer.innerHTML = "";
+    boardContainer.style.display = "grid";
+    boardContainer.style.gridTemplateColumns = "100px 100px 100px";
+    boardContainer.style.gridTemplateRows = "100px 100px 100px";
+    boardContainer.style.gap = "12px";
     for (let i = 0; i < rows; i++) {
       for (let j = 0; j < cols; j++) {
         const cell = document.createElement("div");
-        cell.setAttribute("class", "cells");
-        cell.style.height = "100px";
-        cell.style.width = "100px";
+        cell.innerText = gameBoardArray[i * cols + j];
         cell.style.border = "1px solid black";
-        cell.style.textAlign = "center";
-        cell.style.fontSize = "1em";
+        cell.style.fontSize = "3em";
         cell.style.display = "flex";
         cell.style.alignItems = "center";
+        cell.style.borderRadius = "12px";
         cell.style.justifyContent = "center";
-        cell.innerText = gameBoardArray[i * cols + j];
-        gameContainer.appendChild(cell);
+        cell.setAttribute("class", "cells");
+
+        boardContainer.appendChild(cell);
       }
     }
   };
+  const updateBoard = (position, mark) => {
+    console.log("marked");
+    gameBoardArray[position] = mark;
+  };
+  return {
+    render,
+    updateBoard,
+  };
+})();
 
+//creates instance of new player by giving it name and marker
+const playerFactory = (name, marker) => {
+  return {
+    name,
+    marker,
+  };
+};
 
-  return { render };
+//controls all the game
+const gameController = (function () {
+  players = [];
 
+  const getPlayers = () => {
+    players = [
+      playerFactory(document.getElementById("player1name").value, "X"),
+      playerFactory(document.getElementById("player2name").value, "O"),
+    ];
+  };
+
+  return {
+    getPlayers,
+    players,
+  };
 })();
