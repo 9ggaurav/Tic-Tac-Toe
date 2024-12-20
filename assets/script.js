014b58f84082ec070d111d37c7ec376b8f9b162d
Gaurav Raut <zgaurav713@gmail.com>
2024-12-20 13:13:00 +0530

    start with iife modules and factories

--- assets/script.js
+++ assets/script.js
@@ -1,109 +1,63 @@
-console.log("hell0");
+const Modal = (() => {
+  const modal = document.getElementById("formdialog");
+  const start = document.getElementById("submitstartgame");
+  const gameForm = document.getElementById("gameform");
 
-const boxes = document.querySelectorAll(".box");
-const roundResult = document.getElementById("round-result");
-
-let Board = ["", "", "", "", "", "", "", "", ""];
-let currentMarker = "X";
-let roundOver = false;
-let round = 1;
-
-function switchMarker() {
-  if (currentMarker === "X") {
-    currentMarker = "O";
-  } else {
-    currentMarker = "X";
-  }
-}
-
-function roundStatus(board) {
-  if (
-    (board[0] === board[1] &&
-      board[1] === board[2] &&
-      board[0] != "" &&
-      board[1] != "" &&
-      board[2] != "") ||
-    (board[3] === board[4] &&
-      board[4] === board[5] &&
-      board[3] != "" &&
-      board[4] != "" &&
-      board[5] != "") ||
-    (board[6] === board[7] &&
-      board[7] === board[8] &&
-      board[6] != "" &&
-      board[7] != "" &&
-      board[8] != "") ||
-    (board[0] === board[3] &&
-      board[3] === board[6] &&
-      board[3] != "" &&
-      board[0] != "" &&
-      board[6] != "") ||
-    (board[1] === board[4] &&
-      board[4] === board[7] &&
-      board[1] != "" &&
-      board[4] != "" &&
-      board[7] != "") ||
-    (board[2] === board[5] &&
-      board[5] === board[8] &&
-      board[2] != "" &&
-      board[5] != "" &&
-      board[8] != "") ||
-    (board[0] === board[4] &&
-      board[4] === board[8] &&
-      board[0] != "" &&
-      board[4] != "" &&
-      board[8] != "") ||
-    (board[2] === board[4] &&
-      board[4] === board[6] &&
-      board[2] != "" &&
-      board[4] != "" &&
-      board[6] != "")
-  ) {
-    console.log("someone won");
-    console.log(`${currentMarker} Won the round`);
-    roundOver = true;
-    roundResult.innerText = `${currentMarker} Won the Round! Click anywhere to start new Round`;
-  }
-  else if(!Board.includes("")){
-    console.log("Round Draw");
-    roundOver = true;
-    roundResult.innerText = `Round Draw! Click anywhere to start new Round`;
-
-}
-}
-
-function displayBoard(array) {
-  let i = 0;
-  boxes.forEach((box) => {
-    box.setAttribute("id", `${i}`);
-    box.innerText = `${array[i++]}`;
+  window.addEventListener("DOMContentLoaded", () => {
+    modal.showModal();
   });
-}
 
-function playRound(box) {
-  if (roundOver) {
-    console.log("Round Over!");
-    console.log("Start new Round");
-    Board = ["", "", "", "", "", "", "", "", ""];
-    currentMarker = "X";
-    roundOver = false;
-    displayBoard(Board);
-    round = round + 1;
-    roundResult.innerText = `Round ${round}`;
-  } else {
-    if (Board[box.id] === "") {
-      Board[box.id] = `${currentMarker}`;
-      displayBoard(Board);
-      roundStatus(Board);
-      switchMarker();
-    } else {
-      console.log("Can't play there");
+  const openModal = () => {
+    modal.showModal();
+  };
+
+  start.addEventListener("click", (event) => {
+    event.preventDefault();
+    if(!gameForm.checkValidity()){
+      alert("please fill out the form!");
+      return;
     }
-  }
+    gameForm.remove();
+    modal.close();
+  });
+
+  const closeModal = () => {
+    modal.close();
+  };
+
+  return { openModal, closeModal };
+})();
+
+const playerFactory=function(name, marker){
+  
 }
 
-displayBoard(Board);
+const gameBoard = (() => {
+  const gameContainer = document.getElementById("gameboard");
+  let gameBoardArray = ["X", "", "O", "X", "", "", "", "g", ""];
+  const rows = 3;
+  const cols = 3;
 
-boxes.forEach((box) => {
-  box.addEventListener("click", () => playRound(box));
-});
+  const render = function (gameBoardArray) {
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        const cell = document.createElement("div");
+        cell.setAttribute("class", "cells");
+        cell.style.height = "100px";
+        cell.style.width = "100px";
+        cell.style.border = "1px solid black";
+        cell.style.textAlign = "center";
+        cell.style.fontSize = "1em";
+        cell.style.display = "flex";
+        cell.style.alignItems = "center";
+        cell.style.justifyContent = "center";
+        cell.innerText = gameBoardArray[i * cols + j];
+        gameContainer.appendChild(cell);
+      }
+    }
+  };
+
+
+  return { render };
+
+})();
